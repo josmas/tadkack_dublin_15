27dd169129844f554d829751dcf22143b6e65263
adria <adria@>
2015-06-28 18:24:28 +0200

    fixed shitty go server

--- server/server.go
+++ server/server.go
@@ -4,71 +4,93 @@ import (
     "fmt"
     "log"
     "net/http"
-    "strings"
-    "bytes"
-    "io"
-    "os" 
     "encoding/json"
     "strconv"
+    "time"
+    "sync"
 )
 
-func readfile(filename string) string {
-  buf := bytes.NewBuffer(nil)
-  f, _ := os.Open(filename) // Error handling elided for brevity.
-  io.Copy(buf, f)           // Error handling elided for brevity.
-  f.Close()
-  s := string(buf.Bytes())
-  return s
+type Reciever struct {
+    sip      string
+    lastPing time.Time
+}
+
+func update (recievers []Reciever, addsip *string) []Reciever {
+
+    ret := []Reciever{}
+    now := time.Now()
+
+    for _, s := range recievers {
+
+        if addsip!=nil && s.sip == *addsip {
+           ret = append(ret,Reciever{sip:*addsip,lastPing: now})
+           addsip = nil
+           continue 
+        }
+
+        elapsed := time.Now().Sub(s.lastPing).Seconds()
+        if elapsed < 5 {
+            ret = append(ret,s) 
+        }       
+    }
+
+    if addsip!=nil {
+       ret = append(ret,Reciever{sip:*addsip,lastPing: now}) 
+    }
+
+    return ret      	
+}
+
+func recievers2json(recievers []Reciever) string {
+   list := []string{}
+   for _, r := range recievers {
+      list = append(list,r.sip)
+   }
+   sipm, _ := json.Marshal(list)
+   return string(sipm)
 }
 
 func main() {
-    
-    sips := []string{}
-  
+
+    recievers := []Reciever{}    
+    var mutex = &sync.Mutex{}
+ 
     http.HandleFunc("/tropo", func(w http.ResponseWriter, r *http.Request) {
-      sipcountstr := strconv.Itoa(len(sips))
-      sipm, _ := json.Marshal(sips)     
+      
+      mutex.Lock()
+      recievers = update(recievers,nil)
+      mutex.Unlock()
+      
+      sipcountstr := strconv.Itoa(len(recievers))
+      sipjson:= recievers2json(recievers)     
       res := `{"tropo":[
       {
-         "say":[{"value":"There are `+sipcountstr+` available operators. Transferring you now."}]
-      },
+         "say":[{"value":"There are `+sipcountstr+` available operators. Transferring you now."}      ]},
       {
          "transfer":{
-            "to":`+string(sipm)+`,
+            "to":`+sipjson+`,
             "choices":{"terminator":"#"},
             "on":{
                "say":{"value":"http://www.phono.com/audio/holdmusic.mp3"},
                "event":"ring"
             }
          }
-      }
-   ]}`
-
-
-
-
+      }]}`
 
         fmt.Fprintf(w, "%s", res)
         println(res)
     })
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        html := readfile("phone.html")
-        fmt.Fprintf(w, "%s", html)
+    http.HandleFunc("/api/register", func(w http.ResponseWriter, r *http.Request) {
+        sip := "sip:"+r.URL.Query().Get("sip")
+        mutex.Lock()
+        recievers = update(recievers,&sip)
+        mutex.Unlock()
+        fmt.Fprintf(w,strconv.Itoa(len(recievers)))
     })
 
-    http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
-        sip_ := r.URL.Query().Get("sip")
-        sips_ := strings.Split(sip_,"=")
-        sip := "sip:"+sips_[1]
-        fmt.Fprintf(w,"Success")
-        sips = append(sips,sip)
-        println("SIP LIST ______________")
-        for _, s := range sips {
-            println("-"+s)
-        }
-        println("")
-    })
+    http.Handle("/", http.FileServer(http.Dir("./static/")))
+
     println("Let's go!")  
     log.Fatal(http.ListenAndServe(":8000", nil))
 }
